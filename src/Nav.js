3c4291c9d4d77022effa51ec4bdde45815d72513
joseciudadruiz <joseciudadruiz@gmail.com>
2021-05-25 12:05:21 +0100

    changes to header

--- src/Nav.js
+++ src/Nav.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import {Home, About, Contact} from './Templates';
 import './Nav.css';
 
 const Header = () => {
@@ -17,6 +18,11 @@ const Header = () => {
                     </li>
                 </ul>
                 </nav>
+                <Switch>
+                    <Route path= '/' exact component={Home} />
+                    <Route path='/about/' component={About} />
+                    <Route path='/contact' component={Contact} />
+                </Switch>
             </div>
             
         </Router>
